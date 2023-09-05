08db283099e482d4eab0e0390ed0af2f6365562c
wtorkanorka <wtorkanorka.shuclin@yandex.ru>
2023-09-05 16:22:35 +0500

    Сложно, очень сложно

---
+++ тема 3/урок 10/1.py
@@ -0,0 +1,28 @@
+# 1 https://github.com/wtorkanorka/Synergy-Phyton-learning.git
+pets = dict()
+petName = input("petName :")
+animal = input("animal :")
+age = input("age :")
+lastNumber = int(list(age)[-1])
+syntaxAge = ""
+if int(age) >= 10 and int(age) < 21:
+    syntaxAge = " лет"
+elif lastNumber < 5:
+    syntaxAge = " года"
+elif lastNumber >= 5:
+    syntaxAge = " лет"
+else:
+    syntaxAge = " года"
+
+nameOwner = input("nameOwner :")
+pet_info = {
+    "Вид питомца": animal,
+    "возраст": age + syntaxAge,
+    "Имя хозяина": nameOwner,
+}
+pets[f"{petName}"] = pet_info
+
+
+print(
+    f"Это {pets[petName]['Вид питомца']} по кличке :{petName}, возраст питомца : {pets[petName]['возраст']}, имя владельца : {pets[petName]['Имя хозяина']}"
+)
---
+++ тема 3/урок 10/2.py
@@ -0,0 +1,6 @@
+# 1 https://github.com/wtorkanorka/Synergy-Phyton-learning.git
+my_dict = dict()
+
+for i in range(10, -5 - 1, -1):
+    my_dict[i] = i**i
+print(my_dict)
